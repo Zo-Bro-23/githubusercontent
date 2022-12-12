7467a30e146686076c810583a37e7ff389ce162d
Zohan Subhash <zohan.subhash@gmail.com>
2022-12-12 15:22:46 +0000

    Commit

--- api/index.js
+++ api/index.js
@@ -5,14 +5,18 @@ const app = express()
 app.get('/*', (req, res) => {
     if (!req.query.user || !req.query.repo || !req.query.path) {
         res.status(400).send('Error: Missing parameters (user, repo, path)')
+        return
     }
     axios.get(`https://github.com/${req.query.user}/${req.query.repo}/raw/${req.query.branch || 'main'}/${req.query.path}`)
         .then(response => {
-            res.send(response.data)
+            console.log(response.headers)
+            res.header('Content-Type', response.headers['content-type']).send(response.data)
         })
         .catch(error => {
-            res.status(400).send(`Error: ${error.response.data}`)
+            res.status(400).send(`Error: ${error.response}`)
         })
 })
+
+app.listen(5210)
 
 module.exports = app
